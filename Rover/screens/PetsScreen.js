7903bc5b30e8b63cda1138724055b07dc3d5a5f9
Justin Pinero <justinpinero@gmail.com>
2018-10-15 16:41:29 -0400

    final commit for 2 hour mark

--- Rover/screens/PetsScreen.js
+++ Rover/screens/PetsScreen.js
@@ -6,7 +6,7 @@ export default class PetsScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      cards: cards,
+      cards: [],
       outOfCards: false
     }
   }
